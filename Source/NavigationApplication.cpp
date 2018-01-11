3ad58e4985dae125f311eac2b7163fab740d1ab7
pengjiawei <917527280@qq.com>
2018-01-10 23:09:27 -0800

    first commit

---
+++ Source/NavigationApplication.cpp
@@ -0,0 +1,21 @@
+/*
+ * NavigationApplication.cpp
+ *
+ *  Created on: Jan 10, 2018
+ *      Author: root
+ */
+
+#include "NavigationApplication.h"
+
+namespace NS_Navigation {
+
+NavigationApplication::NavigationApplication() {
+	// TODO Auto-generated constructor stub
+
+}
+
+NavigationApplication::~NavigationApplication() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace NS_Navigation */
---
+++ Source/NavigationApplication.h
@@ -0,0 +1,21 @@
+/*
+ * NavigationApplication.h
+ *
+ *  Created on: Jan 10, 2018
+ *      Author: root
+ */
+
+#ifndef NAVIGATIONAPPLICATION_H_
+#define NAVIGATIONAPPLICATION_H_
+
+namespace NS_Navigation {
+
+class NavigationApplication {
+public:
+	NavigationApplication();
+	virtual ~NavigationApplication();
+};
+
+} /* namespace NS_Navigation */
+
+#endif /* NAVIGATIONAPPLICATION_H_ */
